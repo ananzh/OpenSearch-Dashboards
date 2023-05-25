1915a458e12f3cce9f46339c6db9e983fad519ae
ananzh <ananzh@amazon.com>
2023-05-25 21:14:00 +0000

    remove infinite listener
    Signed-off-by: ananzh <ananzh@amazon.com>

--- scripts/jest_integration.js
+++ scripts/jest_integration.js
@@ -43,7 +43,6 @@
 /* Increase the max listeners
  * Numerous listeners are added by `root.start()` that are never removed.
  */
-process.setMaxListeners(Infinity);
 
 var resolve = require('path').resolve;
 process.argv.push('--config', resolve(__dirname, '../src/dev/jest/config.integration.js'));
